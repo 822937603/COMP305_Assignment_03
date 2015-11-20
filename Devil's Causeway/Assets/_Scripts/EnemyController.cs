90038da1a4fa2c0f024642e8774764ed78383f2c
Jonathan Lee <jonlee50@hotmail.com>
2015-11-19 22:33:14 -0500

    Commit 3
    Skeleton effects and Chest Effects

---
+++ Devil's Causeway/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform player;
+	public float speed;
+
+	public AudioSource skeletonDeathSound;
+	public GameObject skeleton;
+	public GameObject deathFire;
+	public GameObject deathFireClone;
+
+	// Use this for initialization
+	void Start () {
+		skeletonDeathSound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.LookAt (player);
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+	}
+
+	void DestroySkeleton (){
+		Destroy (gameObject);
+	}
+	
+
+	void OnTriggerEnter(Collider otherGameObject) {
+		//Collision detection for colliding with the Player
+		
+		if (otherGameObject.tag == "Player") {
+			skeletonDeathSound.Play();
+			//deathFireClone = (GameObject) 
+			Instantiate(deathFire, skeleton.transform.position, Quaternion.identity);
+			Invoke ("DestroySkeleton",0.5f);
+			//Destroy (deathFireClone, 3.0f);
+			
+			
+			//}
+		}
+	}
+}
---
+++ Devil's Causeway/Assets/_Scripts/ItemController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemController : MonoBehaviour {
+
+	public AudioSource chestCollect;
+	public GameObject chest;
+	public GameObject collectSmoke;
+	public GameObject collectSmokeClone;
+
+	// Use this for initialization
+	void Start () {
+		chestCollect = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void DestroyChest (){
+		Destroy (gameObject);
+	}
+
+	void DestroySmoke () {
+		Destroy (collectSmokeClone);
+	}
+
+	void OnTriggerEnter(Collider otherGameObject) {
+		//Collision detection for colliding with the Player
+
+		if (otherGameObject.tag == "Player") {
+			chestCollect.Play();
+			collectSmokeClone = (GameObject) Instantiate(collectSmoke, chest.transform.position, Quaternion.identity);
+			Invoke ("DestroyChest",0.5f);
+			Destroy (collectSmokeClone, 3.0f);
+
+			
+		//}
+	}
+}
+}
+	
+
